ba2d2b848920545bbcb28d98f28285eef453a643
Amrou Kithkin <amroukithkin@gmail.com>
2019-12-23 12:24:21 -0800

    initial commit of bash scripts

---
+++ goose.sh
@@ -0,0 +1,2 @@
+curl -X POST -F text=HONK http://pegasus.noise:4444/api/text
+curl -X POST -F image=@goosegame.png http://pegasus.noise:4444/api/image
---
+++ parrot.sh
@@ -0,0 +1,1 @@
+curl -X POST -F image=@partyon.gif http://pegasus.noise:4444/api/image
