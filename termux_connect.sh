03a23b6d9d3f1f574dc82f965eff1674cb9dd89e
Amrou Kithkin <amroukithkin@gmail.com>
2020-01-25 14:45:40 -0800

    more sigh

--- termux_connect.sh
+++ termux_connect.sh
@@ -4,10 +4,13 @@ then
 	#load wifi info
 	termux-wifi-scaninfo
 	ssid=$(termux-wifi-connectioninfo | jq ".ssid")
-	if [ "$ssid" = "Noisebridge" ];
+	echo "$ssid"
+	if [ "${ssid//\"}" = "Noisebridge" ];
 	then
 		echo "on Noisebridge WAN, ENGAGE THE PARROTS"
 		sh parrot.sh
 	fi
+else
+    echo "did not find termux"
 fi
-echo "did not find termux"
+
