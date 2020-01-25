c3ffac16ca11d537da2b16e97071dae569b328c9
Amrou Kithkin <amroukithkin@gmail.com>
2020-01-25 14:24:18 -0800

    i am bad at bash

--- termux_connect.sh
+++ termux_connect.sh
@@ -1,10 +1,9 @@
 if which termux-info
 then 
 	echo "found termux"
-
 	#load wifi info
 	termux-wifi-scaninfo
-	ssid=(termux-wifi-connectioninfo | jq ".ssid")
+	ssid=$(termux-wifi-connectioninfo | jq ".ssid")
 	if ssid == "Noisebridge"
 	then
 		echo "on Noisebridge WAN, ENGAGE THE PARROTS"
