4786e494a31bd893c327180a4c8464f0933d3afd
Amrou Kithkin <amroukithkin@gmail.com>
2020-01-25 15:02:55 -0800

    add listener for maximum lols

--- termux_connect.sh
+++ termux_connect.sh
@@ -2,19 +2,23 @@
 
 if which termux-info
 then 
-	echo "found termux"
-	#load wifi info
-	termux-wifi-scaninfo
-	ssid=$(termux-wifi-connectioninfo | jq ".ssid" | xargs)
-	echo $ssid
-	if [ $ssid = "Noisebridge" ];
-	then
-		echo "on Noisebridge WAN, ENGAGE THE PARROTS"
-		sh parrot.sh
-	else
-		echo "$ssid is not Noisebridge"
-	fi
+	while true
+	do
+		echo "found termux, looking for Noisebridge.."
+		#load wifi info
+		termux-wifi-scaninfo
+		ssid=$(termux-wifi-connectioninfo | jq ".ssid" | xargs)
+		if [ $ssid = "Noisebridge" ];
+		then
+			echo "on Noisebridge wifi.  Waiting 1 minute..."
+			sleep 60
+			sh parrot.sh
+			break
+		else
+			echo "$ssid is not Noisebridge, waiting 30 secs and checking again."
+		fi
+	done
 else
-    echo "did not find termux"
+	echo "did not find termux :("
+	break
 fi
-
