6cdeed44b16f966b470a48fc7682ffc216bc44f1
Amrou Kithkin <amroukithkin@gmail.com>
2020-01-25 14:22:19 -0800

    add v0.1 termux script

---
+++ termux_connect.sh
@@ -0,0 +1,13 @@
+if which termux-info
+then 
+	echo "found termux"
+
+	#load wifi info
+	termux-wifi-scaninfo
+	ssid=(termux-wifi-connectioninfo | jq ".ssid")
+	if ssid == "Noisebridge"
+	then
+		echo "on Noisebridge WAN, ENGAGE THE PARROTS"
+		sh parrot.sh
+fi
+echo "did not find termux"
